08927a58a031b5a4a1e52d46c1bf2a90ee54043c
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-06-30 17:47:15 -0500

    initial commit. can return my athlete data from strava. check console

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [activities, setActivities] = useState({})
+
+  //Strava Credentials
+  let clientID = "87760";
+  let clientSecret = "f04605efba73f7c138835354a71ae04033d7dbda";
+
+  // refresh token and call address
+  const refreshToken = "5010cbc23485a5b4c063cbb1266abf570fdddc15";
+  const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`
+  
+  // endpoint for read-all activities. temporary token is added in getActivities()
+  const callActivities = `https://www.strava.com/api/v3/athlete?access_token=`
+
+  // Use refresh token to get current access token
+  useEffect(() => {
+    fetch(callRefresh, {
+      method: 'POST'
+    })
+    .then(res => res.json())
+    .then(result => getActivities(result.access_token))
+  }, [callRefresh])
+
+  // use current access token to call all activities
+  function getActivities(access){
+    // console.log(callActivities + access)
+      fetch(callActivities + access)
+      .then(res => res.json())
+      .then(data => setActivities(data), setIsLoading(prev => !prev))
+      .catch(e => console.log(e))
+  }
+
+  function showActivities(){
+    if(isLoading) return <>LOADING</>
+    if(!isLoading) {
+      console.log(activities)
+      return activities.length
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showActivities()}
     </div>
   );
 }
