bfbb852a25770d7a49463ed1debf0241f8cb592c
Andrew Gelstein <gelstein.andrew@gmail.com>
2022-07-10 19:16:51 -0500

    Added basic eslint and prettier setup

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import Athlete from './Athlete';
+import React, { useState, useEffect } from "react";
+import Athlete from "./Athlete";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [activities, setActivities] = useState({})
+  const [, setIsLoading] = useState(true);
+  const [activities, setActivities] = useState({});
 
   //Strava Credentials
   let clientID = "87760";
-  let clientSecret = "c49267c34da54ebb8ec31802254a92e86920ef39";
+  let clientSecret = "c1902065f114f7fc616ebce6151c833699407803";
 
   // refresh token and call address
   const refreshToken = "c248d263f6ef4cab61eb1c2e19b9eee6a21eb03c";
-  const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`
-  
+  const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`;
+
   // endpoint for read-all activities. temporary token is added in getActivities()
-  const callActivities = `https://www.strava.com/api/v3/athlete?access_token=`
+  const callActivities = `https://www.strava.com/api/v3/athlete?access_token=`;
 
   // Use refresh token to get current access token
   useEffect(() => {
     fetch(callRefresh, {
-      method: 'POST'
+      method: "POST",
     })
-    .then(res => res.json())
-    .then(result => getActivities(result.access_token))
-  }, [callRefresh])
+      .then((res) => res.json())
+      .then((result) => getActivities(result.access_token));
+  }, [callRefresh]);
 
   // use current access token to call all activities
-  function getActivities(access){
+  function getActivities(access) {
     // console.log(callActivities + access)
-      fetch(callActivities + access)
-      .then(res => res.json())
-      .then(data => setActivities(data), setIsLoading(prev => !prev))
-      .catch(e => console.log(e))
+    fetch(callActivities + access)
+      .then((res) => res.json())
+      .then(
+        (data) => setActivities(data),
+        setIsLoading((prev) => !prev)
+      )
+      .catch((e) => console.log(e));
   }
 
   return (
     <div className="App">
-      <Athlete activities={ activities} />
+      <Athlete activities={activities} />
     </div>
   );
 }
--- src/Athlete.js
+++ src/Athlete.js
@@ -1,22 +1,21 @@
 const Athlete = ({ activities }) => {
-
-return (
+  return (
     <div>
-        <p>ID: {activities.id}</p>
-        <p>First Name: {activities.firstname}</p>
-        <p>Last Name: {activities.lastname}</p>
-        <p>Bio: {activities.bio}</p>
-        <p>Weight: {activities.weight}</p>
-        <p>City: {activities.city}</p>
-        <p>State: {activities.state}</p>
-        <p>Country: {activities.country}</p>
-        <p>Premium: {activities.premium}</p>
-        <p>Updated Last: {activities.updated_at}</p>
-        <div className="image-container">
-            <img src={activities.profile} alt={activities.firstname} />
+      <p>ID: {activities.id}</p>
+      <p>First Name: {activities.firstname}</p>
+      <p>Last Name: {activities.lastname}</p>
+      <p>Bio: {activities.bio}</p>
+      <p>Weight: {activities.weight}</p>
+      <p>City: {activities.city}</p>
+      <p>State: {activities.state}</p>
+      <p>Country: {activities.country}</p>
+      <p>Premium: {activities.premium}</p>
+      <p>Updated Last: {activities.updated_at}</p>
+      <div className="image-container">
+        <img src={activities.profile} alt={activities.firstname} />
       </div>
     </div>
-    )
-}
+  );
+};
 
-export default Athlete
+export default Athlete;
